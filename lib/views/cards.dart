62f6c2511804b7200279961fe4ad6db4c9162a10
VashistChint <126398156+VashistChint@users.noreply.github.com>
2023-06-08 12:06:19 +0530

    building of cards

---
+++ lib/views/cards.dart
@@ -0,0 +1,110 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class Cards extends StatefulWidget {
+  const Cards({Key? key}) : super(key: key);
+
+  @override
+  _CardsState createState() => _CardsState();
+}
+
+class _CardsState extends State<Cards> {
+  late List<CardData> cards;
+  int selectedIndex = -1;
+
+  @override
+  void initState() {
+    super.initState();
+    cards = List.generate(
+      10,
+          (index) => CardData('Card ${index + 1}', getRandomColor()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(height: 80.0),
+        Container( // Wrap ListView.builder with Container
+          height: 200.0, // Set a specific height
+          child: ListView.builder(
+            itemCount: cards.length,
+            itemBuilder: (context, index) {
+              final card = cards[index];
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  },
+                  child: AnimatedOpacity(
+                    duration: Duration(milliseconds: 300),
+                    opacity: selectedIndex == index ? 0.0 : 1.0,
+                    child: Card(
+                      elevation: 4,
+                      color: card.color,
+                      child: SizedBox(
+                        width: 200.0, // Width of a normal credit card
+                        height: 180.0, // Height of a normal credit card
+                        child: ListTile(
+                          title: Text(
+                            card.title,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        if (selectedIndex != -1) buildSelectedCard(),
+      ],
+    );
+  }
+
+  Widget buildSelectedCard() {
+    final selectedCard = cards[selectedIndex];
+    return Container(
+      height: 200.0,
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedIndex = -1;
+          });
+        },
+        child: Card(
+          elevation: 4,
+          color: selectedCard.color,
+          child: ListTile(
+            title: Text(
+              selectedCard.title,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Color getRandomColor() {
+    final random = Random();
+    final r = random.nextInt(256);
+    final g = random.nextInt(256);
+    final b = random.nextInt(256);
+    return Color.fromARGB(255, r, g, b);
+  }
+}
+
+class CardData {
+  final String title;
+  final Color color;
+
+  CardData(this.title, this.color);
+}
