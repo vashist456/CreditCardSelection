4d1048cbc74fb689c81e40cbcc32c127c5d92f12
VashistChint <126398156+VashistChint@users.noreply.github.com>
2023-06-09 17:29:11 +0530

    ui for the scren added an elevated button for navigating between pages added list view builder and card selection index adjust the property inside the single child scrollview to enable scrolling

--- lib/views/tab_controller.dart
+++ lib/views/tab_controller.dart
@@ -1,7 +1,6 @@
-import 'package:credit_card_picker/widget/credit_card_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_brand.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
-import 'package:flutter_credit_card/glassmorphism_config.dart';
 
 class CustomTabController extends StatefulWidget {
   const CustomTabController({Key? key}) : super(key: key);
@@ -12,8 +11,11 @@ class CustomTabController extends StatefulWidget {
 
 class _CustomTabControllerState extends State<CustomTabController>
     with SingleTickerProviderStateMixin {
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+  GlobalKey<RefreshIndicatorState>();
+
   late TabController _tabController;
-  bool _showBackView = false;
+  final bool _showBackView = false;
 
   @override
   void initState() {
@@ -27,160 +29,189 @@ class _CustomTabControllerState extends State<CustomTabController>
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: SizedBox(
-        height: 500,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(left: 20),
-              child: const Text(
-                'Credit Card Options',
-                style: TextStyle(fontSize: 30, fontFamily: 'BigAppText'),
-              ),
-            ),
-            const SizedBox(height: 30),
-            Center(
-              child: SizedBox(
-                height: 40,
-                child: TabBar(
-                  controller: _tabController,
-                  isScrollable: true,
-                  labelPadding: const EdgeInsets.symmetric(horizontal: 20),
-                  indicatorColor: Colors.black,
-                  tabs: const [
-                    Tab(
-                      child: Text(
-                        "Home",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                    Tab(
-                      child: Text(
-                        "Card",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Expanded(
-              child: SizedBox(
-                height: 1000, // Adjust the height as needed
-                child: TabBarView(
-                  controller: _tabController,
-                  children: [
-                    RefreshIndicator(
-                      key: _refreshIndicatorKey,
-                      onRefresh: () {  },
-                      child: ListView.builder(
-                        itemCount: 1,
-                        itemBuilder: (context, index) {
-                          return GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _showBackView = !_showBackView;
-                              });
-                            },
-                            child: Container(
-                                height: 300,
-                                width: 300,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: Colors.grey[300],
-                                ),
-                                child: creditCardWidget(
-                                  cardNumber: '1234 5678 9012 3456',
-                                  expiryDate: '12/23',
-                                  cardHolderName: 'John Doe',
-                                  cvvCode: '123',
-                                  isCvvFocused: false,
-                                  backgroundImage: 'assets/card_background.jpg',
-                                  cardBgColor: Colors.black.withOpacity(0.8),
-                                  // glassmorphismConfig: glassmorphismConfig,
-                                  obscureCardNumber: true,
-                                  obscureCardCvv: true,
-                                  isHolderNameVisible: true,
-                                  textStyle: const TextStyle(),
-                                  isChipVisible: true,
-                                  isSwipeGestureEnabled: true,
-                                  animationDuration: const Duration(milliseconds: 300),
-                                  frontCardBorder: const Border(),
-                                  backCardBorder: const Border(),
-                                  onCreditCardWidgetChange: (creditCardBrand) {},
-                                  glassmorphismConfig: null,
-                                  customCardIcons: [],
-                                  frontCardDecoration: null,
-                                  backCardDecoration: null,
-                                );
-
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          _buildCreditCard(color: Colors.blue),
-                          _buildCreditCard(color: Colors.red),
-                          _buildCreditCard(color: Colors.green),
-                          _buildCreditCard(color: Colors.yellow),
-                          _buildCreditCard(color: Colors.orange),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+  Future<void> _handleRefresh() async {
+    // Add your refresh logic here
+    // For example, you can fetch updated data from a server
+    // or update the UI based on new data
   }
 
-  Widget _buildCreditCard({required Color color}) {
-    return Container(
-      height: 200,
-      width: 300,
-      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: color,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Credit Card Options'),
       ),
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: EdgeInsets.all(16),
-            child: Text(
-              'Card Details',
-              style: TextStyle(fontSize: 20, color: Colors.white),
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: const Text(
+              'Credit Card Options',
+              style: TextStyle(fontSize: 30, fontFamily: 'BigAppText'),
             ),
           ),
-          SizedBox(height: 10),
-          Text(
-            'Card Number',
-            style: TextStyle(fontSize: 16, color: Colors.white),
+          const SizedBox(height: 30),
+          Center(
+            child: SizedBox(
+              height: 40,
+              child: TabBar(
+                controller: _tabController,
+                isScrollable: true,
+                labelPadding: const EdgeInsets.symmetric(horizontal: 20),
+                indicatorColor: Colors.black,
+                tabs: const [
+                  Tab(
+                    child: Text(
+                      "Home",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      "Card",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          SizedBox(height: 10),
-          Text(
-            'Card Holder',
-            style: TextStyle(fontSize: 16, color: Colors.white),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Expiry Date',
-            style: TextStyle(fontSize: 16, color: Colors.white),
+          const SizedBox(height: 10),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                RefreshIndicator(
+                  key: _refreshIndicatorKey,
+                  onRefresh: _handleRefresh,
+                  child: ListView.builder(
+                    itemCount: 1,
+                    itemBuilder: (context, index) {
+                      return creditCardWidget(
+                        cardNumber: '1234 5678 9012 3456',
+                        expiryDate: '12/23',
+                        cardHolderName: 'John Doe',
+                        cvvCode: '123',
+                        backgroundImage: 'assets/images/card_background.png',
+                        cardBgColor: Colors.black.withOpacity(0.8),
+                        obscureCardNumber: true,
+                        obscureCardCvv: true,
+                        isHolderNameVisible: true,
+                        textStyle: const TextStyle(),
+                        isChipVisible: true,
+                        isSwipeGestureEnabled: true,
+                        animationDuration: const Duration(milliseconds: 300),
+
+                        onCreditCardWidgetChange: (creditCardBrand) {},
+                        showBackView: _showBackView,
+                        customCardIcons: const [],
+                      );
+                    },
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: [
+                       ...List.generate(10, (index) => GestureDetector(
+                              onTap: () {},
+                              child: creditCardWidget(
+                                cardNumber: '1234 5678 9012 3456',
+                                expiryDate: '12/23',
+                                cardHolderName: 'John Doe',
+                                cvvCode: '123',
+                                backgroundImage: 'assets/images/card_background.png',
+                                cardBgColor: Colors.black.withOpacity(0.8),
+                                obscureCardNumber: true,
+                                obscureCardCvv: true,
+                                isHolderNameVisible: true,
+                                textStyle: const TextStyle(),
+                                isChipVisible: true,
+                                isSwipeGestureEnabled: true,
+                                animationDuration: const Duration(milliseconds: 300),
+                                onCreditCardWidgetChange: (creditCardBrand) {},
+                                showBackView: _showBackView,
+                                customCardIcons: const [],
+                              ),
+                            ),
+
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+Widget creditCardWidget({
+  required String cardNumber,
+  required String expiryDate,
+  required String cardHolderName,
+  required String cvvCode,
+  required String backgroundImage,
+  required Color cardBgColor,
+  required bool obscureCardNumber,
+  required bool obscureCardCvv,
+  required bool isHolderNameVisible,
+  required TextStyle textStyle,
+  required bool isChipVisible,
+  required bool isSwipeGestureEnabled,
+  required Duration animationDuration,
+
+  required Function(CreditCardBrand) onCreditCardWidgetChange,
+  required bool showBackView,
+  required List<CustomCardTypeIcon> customCardIcons,
+}) {
+
+  return SizedBox(
+    height: 175,
+    child: CreditCardWidget(
+      cardNumber: cardNumber,
+      expiryDate: expiryDate,
+      cardHolderName: cardHolderName,
+      cvvCode: cvvCode,
+      cardBgColor: cardBgColor,
+      backgroundImage: backgroundImage,
+      obscureCardNumber: obscureCardNumber,
+      obscureCardCvv: obscureCardCvv,
+      isHolderNameVisible: isHolderNameVisible,
+      textStyle: textStyle,
+      isChipVisible: isChipVisible,
+      isSwipeGestureEnabled: isSwipeGestureEnabled,
+      animationDuration: animationDuration,
+
+      onCreditCardWidgetChange: onCreditCardWidgetChange,
+      showBackView: showBackView,
+
+    ),
+  );
+}
+
+class CustomCardTypeIcon {
+  final CardType cardType;
+  final Widget cardIcon;
+
+  CustomCardTypeIcon({
+    required this.cardType,
+    required this.cardIcon,
+  });
+}
+List<CustomCardTypeIcon> customCardIcons = [
+  CustomCardTypeIcon(
+    cardType: CardType.visa,
+    cardIcon: Icon(Icons.payment),
+  ),
+  CustomCardTypeIcon(
+    cardType: CardType.mastercard,
+    cardIcon: Icon(Icons.payment),
+  ),
+  // Add more custom card type icons if needed
+];
+
+
