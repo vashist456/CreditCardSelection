442c4db1947d5e22fb329f42a9f5910d475558d3
VashistChint <126398156+VashistChint@users.noreply.github.com>
2023-06-09 10:02:35 +0530

    ui for the scren added an elevated button for navigating between pages added list view builder and card selection index

---
+++ lib/views/settings_page.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: prefer_const_constructors
+
+// import 'package:flutter/material.dart';
+//
+// import '../route/route.dart' show homeScreen; // Make sure to import the correct route
+//
+// class SettingsPage extends StatelessWidget {
+//   const SettingsPage({Key? key}) : super(key: key);
+//
+//   void navigateToHomeScreen(BuildContext context) {
+//     Navigator.pushNamed(context, homeScreen);
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Settings Page'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           child: Text('Go to Home Screen'),
+//           onPressed: () => navigateToHomeScreen(context),
+//         ),
+//       ),
+//     );
+//   }
+// }
