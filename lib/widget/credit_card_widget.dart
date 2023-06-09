fc2a9d88a96118f2d6e3c652658bed08d89a627c
VashistChint <126398156+VashistChint@users.noreply.github.com>
2023-06-09 17:25:53 +0530

    ui for the scren added an elevated button for navigating between pages added list view builder and card selection index changed the preferences in the credit card widget class property

--- lib/widget/credit_card_widget.dart
+++ lib/widget/credit_card_widget.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/credit_card_brand.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
+
 Widget creditCardWidget({
   required String cardNumber,
   required String expiryDate,
   required String cardHolderName,
   required String cvvCode,
   required bool isCvvFocused,
-  required String backgroundImage,
-  required Color cardBgColor,
-  required Glassmorphism glassmorphismConfig,
-  required bool obscureCardNumber,
-  required bool obscureCardCvv,
-  required bool isHolderNameVisible,
-  required TextStyle textStyle,
-  required bool isChipVisible,
+  String? backgroundImage,
+  bool? obscureCardNumber,
+  bool? obscureCardCvv,
+  bool? isHolderNameVisible,
+  TextStyle? textStyle,
+  bool? isChipVisible,
   required bool isSwipeGestureEnabled,
-  required Duration animationDuration,
-  required Border frontCardBorder,
-  required Border backCardBorder,
-  required List<CustomCardTypeImage> customCardIcons,
-  required BoxDecoration frontCardDecoration,
-  required BoxDecoration backCardDecoration,
+  BoxBorder? frontCardBorder,
+  BoxBorder? backCardBorder,
   required Function(CreditCardBrand) onCreditCardWidgetChange,
+  required List<Widget> customCardIcons,
+  required BoxDecoration? frontCardDecoration,
+  required BoxDecoration? backCardDecoration,
 }) {
   return SizedBox(
     height: 175,
@@ -31,27 +29,20 @@ Widget creditCardWidget({
       expiryDate: expiryDate,
       cardHolderName: cardHolderName,
       cvvCode: cvvCode,
-
-      cardBgColor: cardBgColor,
-      glassmorphismConfig: glassmorphismConfig,
       backgroundImage: backgroundImage,
-      obscureCardNumber: obscureCardNumber,
-      obscureCardCvv: obscureCardCvv,
-      isHolderNameVisible: isHolderNameVisible,
+      obscureCardNumber: false,
+      obscureCardCvv: true ,
+      isHolderNameVisible: false,
       textStyle: textStyle,
-      isChipVisible: isChipVisible,
-      isSwipeGestureEnabled: isSwipeGestureEnabled,
-      animationDuration: animationDuration,
+      isChipVisible: true,
+      animationDuration: const Duration(milliseconds: 100),
       frontCardBorder: frontCardBorder,
       backCardBorder: backCardBorder,
-
       onCreditCardWidgetChange: onCreditCardWidgetChange,
-      showBackView: false,
+      showBackView: true,
     ),
   );
 }
-
-
 
 class CustomCardTypeImage {
   final CardType cardType;
