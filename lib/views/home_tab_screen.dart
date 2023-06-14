b236dd139fc1901fba2f007f69a7eae65051a947
VashistChint <126398156+VashistChint@users.noreply.github.com>
2023-06-14 11:12:29 +0530

    ui for the scren added an elevated button for navigating between pages added list view builder and card selection index display of credit card with due billing date

---
+++ lib/views/home_tab_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_brand.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+class Hometab extends StatefulWidget {
+  const Hometab({Key? key}) : super(key: key);
+
+  @override
+  State<Hometab> createState() => _HometabState();
+}class _HometabState extends State<Hometab> {
+  String cardNumber = '1234 5678 9012 3456';
+  String cardHolderName = 'John Doe';
+  String cvvNumber = '123';
+  String expiryDate = '12/24';
+  bool showBackView = false;
+
+  void toggleCardView() {
+    setState(() {
+      showBackView = !showBackView;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Tab'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: toggleCardView,
+              child: CreditCardWidget(
+                bankName: 'American Express',
+                cardNumber: cardNumber,
+                expiryDate: expiryDate,
+                cardHolderName: cardHolderName,
+                cvvCode: cvvNumber,
+                showBackView: showBackView,
+                cardBgColor: Colors.greenAccent,
+                textStyle: const TextStyle(
+                  color: Colors.black, // Set the font color to black
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                animationDuration: const Duration(milliseconds: 1200),
+                onCreditCardWidgetChange: (CreditCardBrand brand) {},
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Pending Amount: \$100',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              'Due Date: June 30, 2023',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
