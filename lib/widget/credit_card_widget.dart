4b22aea4c65ed859891a3e5fe182d94f5ea5f286
VashistChint <126398156+VashistChint@users.noreply.github.com>
2023-06-16 07:12:24 +0530

    ui for the scren added an elevated button for navigating between pages added list view builder and card selection index implemented a credit card for display innside a column used the method void onCr3editCardModelChange method

--- lib/widget/credit_card_widget.dart
+++ lib/widget/credit_card_widget.dart
@@ -2,54 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/credit_card_brand.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
 
-Widget creditCardWidget({
-  required String cardNumber,
-  required String expiryDate,
-  required String cardHolderName,
-  required String cvvCode,
-  required bool isCvvFocused,
-  String? backgroundImage,
-  bool? obscureCardNumber,
-  bool? obscureCardCvv,
-  bool? isHolderNameVisible,
-  TextStyle? textStyle,
-  bool? isChipVisible,
-  required bool isSwipeGestureEnabled,
-  BoxBorder? frontCardBorder,
-  BoxBorder? backCardBorder,
-  required Function(CreditCardBrand) onCreditCardWidgetChange,
-  required List<Widget> customCardIcons,
-  required BoxDecoration? frontCardDecoration,
-  required BoxDecoration? backCardDecoration,
-}) {
-  return SizedBox(
-    height: 175,
-    child: CreditCardWidget(
-      cardNumber: cardNumber,
-      expiryDate: expiryDate,
-      cardHolderName: cardHolderName,
-      cvvCode: cvvCode,
-      backgroundImage: backgroundImage,
-      obscureCardNumber: false,
-      obscureCardCvv: true ,
-      isHolderNameVisible: false,
-      textStyle: textStyle,
-      isChipVisible: true,
-      animationDuration: const Duration(milliseconds: 100),
-      frontCardBorder: frontCardBorder,
-      backCardBorder: backCardBorder,
-      onCreditCardWidgetChange: onCreditCardWidgetChange,
-      showBackView: true,
-    ),
-  );
+class CreditCard extends StatefulWidget {
+  const CreditCard({Key? key}) : super(key: key);
+
+  @override
+  State<CreditCard> createState() => _CreditCardState();
 }
 
-class CustomCardTypeImage {
-  final CardType cardType;
-  final Widget cardImage;
+class _CreditCardState extends State<CreditCard> {
+  String cardNumber = '';
+  String cardHolderName = '';
+  String cvvNumber = '';
+  String expiryDate = '';
+  bool showBackView = false;
+  bool cvvFocused = true;
 
-  CustomCardTypeImage({
-    required this.cardType,
-    required this.cardImage,
-  });
+  void onCreditCardModelChange(CreditCardModel creditCardModel) {
+    setState(() {
+      cardNumber = creditCardModel.cardNumber;
+      cardHolderName = creditCardModel.cardHolderName;
+      expiryDate = creditCardModel.expiryDate;
+      cvvNumber = creditCardModel.cvvCode;
+      showBackView = creditCardModel.isCvvFocused;
+      cvvFocused = creditCardModel.isCvvFocused;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            cvvFocused = !cvvFocused;
+          });
+        },
+        child: Column(
+          children: [
+            CreditCardWidget(
+              cardNumber: cardNumber,
+              expiryDate: expiryDate,
+              cardHolderName: cardHolderName,
+              cvvCode: cvvNumber,
+              showBackView: showBackView,
+              cardBgColor: Colors.greenAccent,
+              textStyle: const TextStyle(
+                color: Colors.white, // Set text color to white
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+              animationDuration: const Duration(milliseconds: 1200),
+              onCreditCardWidgetChange: (CreditCardBrand brand) {},
+            ),
+            if (cvvFocused)
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Text(
+                  cvvNumber,
+                  style: const TextStyle(
+                    color: Colors.white, // Set text color to white
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
 }
